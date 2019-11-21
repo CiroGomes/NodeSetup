760b81c6d18d9ca4a76ce9e0cb169a545f64c79b
Ciro da Silva Gomes <c-iro@hotmail.com>
2019-11-21 01:01:47 -0300

    Update to use method google.admin instead of axios

--- src/app/controllers/myController.js
+++ src/app/controllers/myController.js
@@ -1,16 +1,41 @@
-import {google} from 'googleapis';
-import api from '../../services/gaxiosConfig'
+import { google } from "googleapis";
+// import api from "../../services/gaxiosConfig";
+import fs from "fs";
 
-class MyController{
-  async method(req, res){
-    const data = '2019-11-01';
-    res.send(req.aceessToken)
-    const config = {
-      headers: {'Authorization': "Bearer " + req.aceessToken, 'Accept': 'application/json'}
-    };
-    const response  = await api.get(`/usage/users/all/dates/${data}`,
-    config)
-    res.send(response)
+const TOKEN_PATH = "store/token.json";
+
+class MyController {
+  async method(req, res) {
+    const data = "2019-11-01";
+
+    const auth = req.oAuth2Client;
+
+
+    const service = google.admin({ version: "reports_v1", auth });
+    service.activities.list(
+      {
+        userKey: "all",
+        applicationName: "login",
+        maxResults: 10
+      },
+      (err, resReports) => {
+        if (err)
+          return console.error("The API returned an error:", err.message);
+
+        const activities = resReports.data.items;
+        if (activities.length) {
+          console.log("Logins:");
+          res.send(activities);
+          activities.forEach(activity => {
+            console.log(
+              `${activity.id.time}: ${activity.actor.email} (${activity.events[0].name})`
+            );
+          });
+        } else {
+          console.log("No logins found.");
+        }
+      }
+    );
   }
 }
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,74 @@
-import { google }  from 'googleapis';
-import axios from 'axios';
+import { google } from "googleapis";
+import fs from "fs";
+import readline from "readline"
 
-import key from '../../store/auth.json';
-const scopes = 'https://www.googleapis.com/auth/admin.reports.usage.readonly';
-const jwt = new google.auth.JWT(key.client_email, null, key.private_key, scopes);
-const view_id = 'XXXXXXX'
+// import key from '../../store/auth.json';
+const SCOPES = [
+  "https://www.googleapis.com/auth/admin.reports.audit.readonly",
+  "https://www.googleapis.com/auth/admin.reports.usage.readonly"
+];
+// const jwt = new google.auth.JWT(key.client_email, null, key.private_key, scopes);
+// const view_id = 'XXXXXXX';
 
-process.env.GOOGLE_APPLICATION_CREDENTIALS = '../../store/auth.json'
+const TOKEN_PATH = "store/token.json";
+const CREDENTIALS_PATH = "store/credentials.json";
 
 export default async (req, res, next) => {
-jwt.authorize((err,response) => {
+  fs.readFile(CREDENTIALS_PATH, (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content), req, res, next);
+  });
+};
+
+function authorize(credentials, req, res, next) {
+  const { client_id, client_secret, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, req, res, next);
     
-      const token = response
-      try {
-        req.aceessToken = token.access_token;
-        
-        return next();
-      } catch (err) {
-        return res.status(401).json({ error: 'Token invalid' });
-      }
-})
+    oAuth2Client.credentials = JSON.parse(token);
+    req.oAuth2Client = oAuth2Client;
+    return next();
+  });
 }
 
+function getAccessToken(oAuth2Client, req, res, next) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("Enter the code from that page here: ", code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error retrieving access token", err);
+
+      // Store the token to disk for later program executions
+      console.log(token)
+      storeToken(token);
+      oAuth2Client.credentials = token;
+      req.oAuth2Client = oAuth2Client;
+      return next();
+    });
+  });
+}
+
+function storeToken(token) {
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+    if (err) return console.warn(`Token not stored to ${TOKEN_PATH}`, err);
+    console.log(`Token stored to ${TOKEN_PATH}`);
+  });
+}
+
+// process.env.GOOGLE_APPLICATION_CREDENTIALS = '../../store/auth.json';
